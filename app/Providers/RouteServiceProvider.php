be54b9da801e6479f7d2c42ab24b388105d18e3d
小菜鸟阿飞 <2686558013@qq.com>
2022-03-22 13:42:05 +0800

    修改跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';///home
     protected  $namespace =  'App\\Http\\Controllers'; // 添加这一行
 
     /**
