67f512ebb157f715fa3d0ae05002ffba3ee5596a
小菜鸟阿飞 <2686558013@qq.com>
2022-03-23 16:47:14 +0800

    填充用户和话题数据

--- database/migrations/2022_03_23_143132_create_topics_table.php
+++ database/migrations/2022_03_23_143132_create_topics_table.php
@@ -10,7 +10,11 @@ return new class extends Migration
 	{
 		Schema::create('topics', function(Blueprint $table) {
             $table->increments('id');
-            
+            $table->text('title')->nullable()->comment('标题');
+            $table->text('body')->nullable()->comment('内容');
+            $table->text('excerpt')->nullable()->comment('摘录');
+            $table->integer('user_id')->unsigned()->comment('用户');
+            $table->integer('category_id')->unsigned()->comment('分类');
             $table->timestamps();
         });
 	}
