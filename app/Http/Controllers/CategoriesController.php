b30421773210b49e458297f4b23afb1e55ccce98
小菜鸟阿飞 <2686558013@qq.com>
2022-03-24 11:23:31 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -12,9 +12,10 @@ class CategoriesController extends Controller
     public function show(Request $request,Topic $topic,Category $category){
         //读取分类 ID关联的话题，并按20条分页
         $topics = $topic->withOrder($request->order)
+                        ->with('user', 'category')   // 预加载防止 N+1 问题
                         ->where('category_id',$category->id)
                         ->paginate(20);
-            
+
         //传参变量话题和分类到模板中
         return view('topics.index',compact('topics','category'));
     }
