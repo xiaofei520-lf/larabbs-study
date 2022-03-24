7d505e87d7446e50b4222fe9c7addf3c9c32d0b5
小菜鸟阿飞 <2686558013@qq.com>
2022-03-24 09:47:22 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -5,6 +5,9 @@ function route_class()
     //把当前路由名称的。换成-
     return str_replace('.','-', Route::currentRouteName());
 }
+function category_nav_active($category_id){
+    return active_class(if_route('categories.show') && if_route_param('category',$category_id));
+}
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -44,5 +44,7 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //个人页面
 Route::resource('users','UsersController',['only'=>['show',
     'update','edit']]);
-
+//帖子
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//分类 话题
+Route::resource('categories','CategoriesController',['only'=>['show']]);
